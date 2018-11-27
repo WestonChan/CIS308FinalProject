8a5570c9d31b0ebea895681dbb153ab91882af73
Cale Povilonis <calepovilonis@cougar.cs.ksu.edu>
2018-11-27 13:57:06 -0600

    edited date files

--- Date.c
+++ Date.c
@@ -6,3 +6,18 @@
 //
 
 #include "Date.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+Date * new_date(Event event[48], int day, Month month, int year) {
+	if  (event == NULL || month == NULL || (day < 0 && day > 31) || year < 0) { return NULL;}
+	
+	Date * d = malloc(sizeof(Date));
+	d->event = event;
+	d->month = month;
+	d->day = day;
+	d->year = year;
+	return d;
+}
+
