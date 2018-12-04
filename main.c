59b59c89a0e7ba164bd7b24e1e3e7b0d2941f329
Weston Chan <wwchan@cougar.cs.ksu.edu>
2018-12-03 21:22:15 -0600

    I borke it

--- main.c
+++ main.c
@@ -6,6 +6,7 @@
 //
 
 #include <stdlib.h>
+#include <string.h>
 #include "Date.h"
 #include "Event.h"
 #include "LinkedList.h"
@@ -37,17 +38,80 @@ void export(LinkedList * l) {
 
 LinkedList * import() {
 	FILE * fp = fopen("Schedule.rofl", "r");
+	char temp[999999999999];
+	char * token;
+	fscanf(fp, "%s", temp);
+
+	token = strtok(temp, "^");
+	strcpy(typeNames[0], token);
+
+	token = strtok(NULL, "^");
+	strcpy(typeNames[1], token);
+
+	token = strtok(NULL, "^");
+	strcpy(typeNames[2], token);
 	
+	LinkedList * prev = NULL;
+	LinkedList * first;
+	int line = -1;
+	Event * event[3][24];
+	int day, month, year;
 	
+	while(fscanf(fp, "%s", temp) != EOF) {/*
+		if(line == -1){
+			token = strtok(temp, "^");
+			day = atoi(token);
 
+			token = strtok(NULL, "^");
+			month = atoi(token);
 
+			token = strtok(NULL, "^");
+			year = atoi(token);
 
+		
+		} else if (line >= 0){
+				token = strtok(temp, "^");
+				int type = atoi(token);
+	
+				token = strtok(NULL, "^");
+				int start = atoi(token);
+	
+				token = strtok(NULL, "^");
+				int end = atoi(token);
+	
+				token = strtok(NULL, "^");
+				char title[TITLELENGTH];
+				strcpy(title, token);
 
+				token = strtok(NULL, "^");
+				char desc[DESCLENGTH];
+				strcpy(desc, token);
+
+				Event * e = new_Event(type, start, end, title, desc);
+	
+				event[type][line%24] = e;
+
+			}*/
+		
+		if(line == 71) {
+			LinkedList * cur = malloc(sizeof(LinkedList));
+			Date * d = new_date(event, day, month, year);
+			cur -> day = d;
+			line = -2;
+			if(prev == NULL) first = cur;
+			if(prev != NULL) prev = prev -> next;
+				prev = cur;
+			
+		}
+		else line++;
+	}
 	fclose(fp);
-	return NULL;
+	return first;
+	LinkedList * cur = malloc(sizeof(LinkedList));
+return cur;
 }
 
-int main(int argc, const char * argv[]) {
+int main(int argc, const char * argv[]) {/*
 	setTypeNames("Calendar0", "Calendar1", "Calendar2");
 	Event * e = new_Event(2,3,24, "Lunch", "A midday meal.");
 
@@ -76,7 +140,14 @@ int main(int argc, const char * argv[]) {
 	l2 -> day = day;
 	l -> next = l2;
 
-	export(l);
+	export(l);*/
+	LinkedList * l = import();
+	while(l != NULL) {
+		char str[10000];
+		toDateString(l -> day, str);
+		l = l -> next;
+		printf("%s\n", str);
+	}	
 	
     return 0;
 }
